c7709f850e6021132e2b1ad2c1cda6f3919bb2dc
dethMastery <me@suphakit.net>
2023-01-12 16:39:27 +0700

    feat: setting up Landing Page

--- src/page.js
+++ src/page.js
@@ -1,7 +1,56 @@
-function page (app, root) {
+let { html } = require('./api/htmlBase')
 
+let style = `
+  <link rel="stylesheet" href="https://cdn.katsuragi.cyou/fonts/TH/LINEseed-Regular" />
+  <link rel="stylesheet" href="https://dethmastery.github.io/katsuStrap/cdn/katsuStrap.css" />
+
+  <style>
+    body, a, div, p, span, h1 {
+      font-family: "LINEseed-Regular", sans-serif !important;
+    }
+    
+    #App, .flex {
+      height: 100vh !important;
+    }
+
+    .card-body p a {
+      color: #2e2f2f;
+      transition: all .3s;
+    }
+
+    .card-body p a:hover {
+      text-decoration: none;
+      opacity: .8;
+    }
+  </style>
+`
+
+function pageRun (app, root) {
+  app.get('/', (req, res) => {
+    let title = "ogIMG Generator"
+
+    let body = `
+      <div class="flex f-middle">
+        <div class="card">
+          <div class="card-header">
+            <h1>ogIMG Generator</h1>
+          </div>
+          <div class="card-body">
+            <p>
+              but there nothing here :3
+            </p>
+            <p>
+              if u want to use just using this <a href="./generate">form</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    `
+
+    res.send(html(title, body, style))
+  })
 }
 
 module.exports = {
-  page
+  pageRun
 }
