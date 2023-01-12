2a3609bd4ffbb3f1917755f92d357199037be27f
dethMastery <me@suphakit.net>
2023-01-12 14:22:24 +0700

    feat: setting up cors

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,12 @@
 let cors = require('cors')
 
-let corsOptions = 
+let corsSettings = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
 
 function middlewareRun(app, exp, root) {
-
-
+  app.use(cors(corsSettings))
   app.use(exp.static('static'))
 }
 
