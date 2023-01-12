7fc9acb7ee5c35838acf16c518985fe899983dfc
dethMastery <me@suphakit.net>
2023-01-12 13:49:26 +0700

    feat: setting appjs frame

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let exp = require('express')
 
+let {middlewareRun} = require('./src/middleware')
+let {pageRun} = require('./src/page')
 let {portRun} = require('./src/port')
 
 let app = exp()
 
+middlewareRun(app, exp, __dirname)
+pageRun(app, __dirname)
 portRun(app, 1980)
