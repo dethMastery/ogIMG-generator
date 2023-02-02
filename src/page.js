dd28853846d31abd5e634780aee449758b78be37
Suphakit P <me@suphakit.net>
2023-02-02 07:47:18 +0700

    added: generation page

--- src/page.js
+++ src/page.js
@@ -7,7 +7,35 @@ function pageRun (app, root) {
     res.send(html(index.title, index.body, style))
   })
 
-  app.get('/canvas', (req, res) => {
+  app.get('/generate', (req, res) => {
+    let body = `<div style="margin: 5rem 0; justify-content: center; text-align: center;">
+    <div class="card" style="min-width: 20rem; max-width: 50rem; margin: 0 auto;">
+      <h1>Preview</h1>
+      <img src="./api/canvas/?url=${req.query.url}&desp=${req.query.desp}" alt="preview">
+      <h2>
+        Link && Usage
+      </h2>
+      <h3>
+        Link
+      </h3>
+      <p>
+        ${req.protocol + '://' + req.get('host') + '/' + `/api/canvas/?url=${req.query.url}&desp=${req.query.desp.replaceAll(' ', '+')}`}
+      </p>
+      <h3>
+        Usage
+      </h3>
+      <p>
+        Just place this line of code in the 'Head' tag in HTML
+      </p>
+      <p class="code">
+        &lt;meta property="og:image" content="${req.protocol + '://' + req.get('host') + '/' + `/api/canvas/?url=${req.query.url}&desp=${req.query.desp.replaceAll(' ', '+')}`}" /&gt;
+      </p>
+    </div>
+  </div>`
+    res.send(html(index.title, body, style))
+  })
+
+  app.get('/api/canvas', (req, res) => {
     canvasBoard(req, res)
   })
 }
--- src/source/pageSource.js
+++ src/source/pageSource.js
@@ -11,6 +11,19 @@ let style = `
       height: 100vh !important;
     }
 
+    .card img {
+      width: 100%;
+      height: auto;
+      border-radius: 1rem;
+    }
+
+    .code {
+      background-color: rgba(0, 0, 0, 0.5);
+      padding: .5rem 0;
+      color: whitesmoke;
+      border-radius: 1rem;
+    }
+
     .card-body p a {
       color: #2e2f2f;
       transition: all .3s;
@@ -20,26 +33,62 @@ let style = `
       text-decoration: none;
       opacity: .8;
     }
+
+    .input-form {
+      margin: 1rem 0;
+    }
+
+    .input-form input {
+      text-align: center;
+      padding: .5rem;
+      margin: .5rem 0;
+      border: 2px solid transparent;
+      border-radius: 1rem;
+      background-color: #2e2f2f;
+      color: white;
+      transition: all .3s;
+    }
+
+    .input-form input:focus {
+      outline: none;
+      border: 2px solid #ff6f61;
+    }
+
+    .input-form button {
+      padding: 1rem;
+      background-color: #ff6f61;
+      color: whitesmoke;
+      margin-top: 1rem;
+      border-radius: 1rem;
+      border: 0;
+      transition: all .3s;
+    }
+
+    .input-form button:hover {
+      transform: scale(1.3);
+      cursor: pointer;
+    }
   </style>
 `
 
 let index = {
   title: 'ogIMG Generator',
   body: `<div class="flex f-middle">
-  <div class="card">
-    <div class="card-header">
-      <h1>ogIMG Generator</h1>
+    <div class="card">
+      <div class="card-header">
+        <h1>ogIMG Generator</h1>
+      </div>
+      <div class="card-body">
+        <form class="input-form" method="GET" action="/generate">
+          <input type="text" name="url" placeholder="Enter your URL" /> <br>
+          <input type="text" name="desp" placeholder="Enter your description" /> <br>
+          <button type="submit">
+            Generate
+          </button>
+        </form>
+      </div>
     </div>
-    <div class="card-body">
-      <p>
-        but there nothing here :3
-      </p>
-      <p>
-        if u want to use just using this <a href="./generate">form</a>
-      </p>
-    </div>
-  </div>
-</div>`
+  </div>`
 }
 
 module.exports = {
